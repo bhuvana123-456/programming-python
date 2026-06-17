86753b999ed8a2868a4d71a04b8dd73b7926d372
RAYALA BHUVANESWARI <95062181+bhuvana123-456@users.noreply.github.com>
2026-06-17 13:15:57 +0530

    Create Percentage_Calculator.py

---
+++ Percentage_Calculator.py
@@ -0,0 +1,6 @@
+obtained_marks = float(input("Enter obtained marks: "))
+maximum_marks = float(input("Enter maximum marks: "))
+
+percentage = (obtained_marks / maximum_marks) * 100
+
+print("Percentage =", percentage, "%")
