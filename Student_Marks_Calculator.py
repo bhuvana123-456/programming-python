afe62aae4c7c5e43d20d13d60ab1d9822c0b555c
RAYALA BHUVANESWARI <95062181+bhuvana123-456@users.noreply.github.com>
2026-06-18 12:49:10 +0530

    Create Student_Marks_Calculator.py

---
+++ Student_Marks_Calculator.py
@@ -0,0 +1,7 @@
+s1 = float(input("Subject 1 Marks: "))
+s2 = float(input("Subject 2 Marks: "))
+s3 = float(input("Subject 3 Marks: "))
+s4 = float(input("Subject 4 Marks: "))
+s5 = float(input("Subject 5 Marks: "))
+total = s1 + s2 + s3 + s4 + s5
+print("Total Marks =", total)
